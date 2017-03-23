8aee2d697014a54172f588d71768dd92b69a44f4
SamAlexanderPerez <9sperez@stpatrick.org>
2017-03-23 13:56:37 -0500

    Additional work.

--- click.js
+++ click.js
@@ -6,14 +6,23 @@ function samecolor(){
   for(let h=0;h<section.length;h++){
     section[h].style.backgroundColor=color;
   }
-  section[0].stlyle.opacity= 0.4;
-  section[1].stlyle.opacity= 0.2;
-  section[2].stlyle.opacity= 0.1;
-  section[3].stlyle.opacity= 0.3;
+  //section[0].stlyle.opacity= 0.4;
+  //section[1].stlyle.opacity= 0.2;
+  //section[2].stlyle.opacity= 0.1;
+  //section[3].stlyle.opacity= 0.3;
 }
 //declare diffcolor() here
 function diffcolor(){
   const doc = document;
+  let color = "blue";
+  let complement = "yellow";
   let div = doc.querySelectorAll("#diffcolor div");
-  //div.style.backgroundColor =
+  div[0].style.backgroundColor=color;
+  div[1].style.backgroundColor=complement;
+  div[2].style.backgroundColor=color;
+  div[3].style.backgroundColor=complement;
+  div[4].style.backgroundColor=color;
+  div[5].style.backgroundColor=complement;
+  div[6].style.backgroundColor=color;
+  div[7].style.backgroundColor=complement;
 }
---
+++ resize.js
@@ -0,0 +1,13 @@
+//Declare tabletSize() here
+function tabletSize(){
+  const doc = document;
+  let sections = doc.querySelectorAll("section div");
+  let width = section.style.width;
+  let height = section.style.height;
+  for(let d=0; d<sections.length;d++){
+
+  }
+}
+
+
+//Declare phoneSize() here
